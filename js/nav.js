d3fe80e09f38deac4ea675ee6128373ea1c9f0a1
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-02 10:27:23 +0200

    added functionality for fixed nav on scroll

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,13 @@
+const header = document.getElementById('header');
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 const navItemLinks = document.querySelectorAll('.nav-item__link');
 
+window.onscroll = () => {
+  scrollY > 10 && window.innerWidth > 640
+    ? header.classList.add('sticky-nav')
+    : header.classList.remove('sticky-nav');
+};
 hamburger.addEventListener('click', toggleNavLinks);
 
 function toggleNavLinks() {
@@ -12,7 +18,10 @@ function toggleNavLinks() {
 navItemLinks.forEach((navItemLink) =>
   navItemLink.addEventListener('click', closeNavLinks)
 );
+
 function closeNavLinks() {
   hamburger.classList.remove('active');
   navLinks.classList.remove('active');
 }
+
+window.addEventListener('resize', closeNavLinks);
