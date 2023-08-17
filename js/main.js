cd227a62cf022e025952004ab02d497ce0e25e02
Christian Predoianu <christianpredoianu@yahoo.com>
2023-08-17 14:07:43 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import nav from './components/nav';
 import darkMode from './darkMode';
 import scrollToTop from './components/scrollToTop';
-import { verticalSwiper } from './components/verticalSwiper';
-import { cubeSwiper1, cubeSwiper2 } from './components/cubeSwiper';
+import './components/verticalSwiper';
+import './components/cubeSwiper';
 import gsap from './gsap';
 import scrollTrigger from './scrollTrigger';
 
