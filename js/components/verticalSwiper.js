2fd108e4ddc8ad9ac94d5791c9267840281a5b8d
Christian Predoianu <christianpredoianu@yahoo.com>
2023-08-22 12:38:03 +0200

    removed autoplay on user interaction

--- js/components/verticalSwiper.js
+++ js/components/verticalSwiper.js
@@ -2,8 +2,8 @@ export const verticalSwiper = new Swiper('.verticalSwiper', {
   direction: 'vertical',
   grabCursor: true,
   autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
+    delay: 5000,
+    disableOnInteraction: true,
   },
   pagination: {
     el: '.swiper-pagination',
