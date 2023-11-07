10674eabd7547a9d2c1e4bb6de8e27140df756d2
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-07 13:44:11 +0100

    refactored + added diff color to ham when nav is open

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,18 +1,32 @@
 const header = document.getElementById('header');
 const hamburger = document.getElementById('hamburger');
+const hamburgerBars = document.querySelectorAll('span.hamburger__bar ');
 const navLinks = document.getElementById('nav-links');
 const navItemLinks = document.querySelectorAll('.nav-item');
 
 export default () => {
-  window.addEventListener('scroll', () => {
-    scrollY > 10 && window.innerWidth > 640
+  function handleStickyNav() {
+    return scrollY > 10 && window.innerWidth > 640
       ? header.classList.add('sticky-nav')
       : header.classList.remove('sticky-nav');
-  });
+  }
+
+  function handleHamburgerColor() {
+    if (navLinks.classList.contains('active')) {
+      hamburgerBars.forEach((hamburgerBar) => {
+        hamburgerBar.style.backgroundColor = 'hsl(0, 0%, 85%)';
+      });
+    } else {
+      hamburgerBars.forEach((hamburgerBar) => {
+        hamburgerBar.style.backgroundColor = '';
+      });
+    }
+  }
 
   function toggleNavLinks() {
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
+    handleHamburgerColor();
   }
 
   function closeNavLinks() {
@@ -33,6 +47,9 @@ export default () => {
     })
   );
 
+  window.addEventListener('scroll', () => {
+    handleStickyNav();
+  });
   window.addEventListener('resize', closeNavLinks);
   hamburger.addEventListener('click', toggleNavLinks);
 };
