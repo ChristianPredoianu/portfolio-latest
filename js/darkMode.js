897093b0c40ce74cf54f2d0b9a0424aaa026bbc5
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-16 11:56:31 +0100

    exported func

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,61 +1,47 @@
-import {
-  removeCubeSwiperShadow,
-  addCubeSwiperShadow,
-} from './components/cubeSwiper';
+import { removeCubeSwiperShadow, addCubeSwiperShadow } from './components/cubeSwiper';
 
 const darkModeCheckbox = document.getElementById('dark-mode-checkbox');
 const darkModeToggler = document.getElementById('dm-toggler');
 const body = document.querySelector('body');
 
-export default () => {
-  window.addEventListener('load', () => {
-    setUserPreferedTheme();
-    watchPreferedThemeChange();
-  });
+export function setUserPreferedTheme() {
+  const isPreferedDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches;
 
-  function setUserPreferedTheme() {
-    const isPreferedDarkMode = window.matchMedia(
-      '(prefers-color-scheme:dark)'
-    ).matches;
-
-    if (isPreferedDarkMode) {
-      enableDarkMode();
-      removeCubeSwiperShadow();
-    } else {
-      disableDarkMode();
-      addCubeSwiperShadow();
-    }
-  }
-
-  function watchPreferedThemeChange() {
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', function (e) {
-        const colorScheme = e.matches ? 'dark' : 'light';
-
-        colorScheme === 'dark' ? enableDarkMode() : disableDarkMode();
-      });
-  }
-
-  function toggleDarkMode() {
-    darkModeToggler.addEventListener('click', () => {
-      darkModeCheckbox.checked = !darkModeCheckbox.checked;
-
-      darkModeCheckbox.checked ? enableDarkMode() : disableDarkMode();
-    });
-  }
-
-  function enableDarkMode() {
-    body.classList.add('darkmode');
-    darkModeCheckbox.checked = true;
+  if (isPreferedDarkMode) {
+    enableDarkMode();
     removeCubeSwiperShadow();
-  }
-
-  function disableDarkMode() {
-    body.classList.remove('darkmode');
-    darkModeCheckbox.checked = false;
+  } else {
+    disableDarkMode();
     addCubeSwiperShadow();
   }
+}
 
-  toggleDarkMode();
-};
+export function watchPreferedThemeChange() {
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', function (e) {
+      const colorScheme = e.matches ? 'dark' : 'light';
+
+      colorScheme === 'dark' ? enableDarkMode() : disableDarkMode();
+    });
+}
+
+export function toggleDarkMode() {
+  darkModeToggler.addEventListener('click', () => {
+    darkModeCheckbox.checked = !darkModeCheckbox.checked;
+
+    darkModeCheckbox.checked ? enableDarkMode() : disableDarkMode();
+  });
+}
+
+function enableDarkMode() {
+  body.classList.add('darkmode');
+  darkModeCheckbox.checked = true;
+  removeCubeSwiperShadow();
+}
+
+function disableDarkMode() {
+  body.classList.remove('darkmode');
+  darkModeCheckbox.checked = false;
+  addCubeSwiperShadow();
+}
