1b17ef750225ab04cce4605316cf6f66656924ea
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-16 13:06:18 +0100

    final

--- js/gsap.js
+++ js/gsap.js
@@ -47,7 +47,7 @@ export default () => {
   }
 
   function setAnimationEndToSessionStorage() {
-    /* sessionStorage.setItem('isHeroAnimated', true); */
+    sessionStorage.setItem('isHeroAnimated', true);
   }
 
   //Hero section floating icons
