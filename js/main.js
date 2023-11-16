cdb6c5221e917fb68caecf85a3c8d497a3bd7621
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-16 11:56:11 +0100

    added eventListeners

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
-import nav from './components/nav';
-import darkMode from './darkMode';
-import scrollToTop from './components/scrollToTop';
-import './components/verticalSwiper';
-import './components/cubeSwiper';
+import {
+  toggleDarkMode,
+  setUserPreferedTheme,
+  watchPreferedThemeChange,
+} from './darkMode';
+import { handleStickyNav, closeNavLinks } from './components/nav';
+import { verticalSwiper } from './components/verticalSwiper';
+import { cubeSwiper1, cubeSwiper2 } from './components/cubeSwiper';
 import gsap from './gsap';
 import scrollTrigger from './scrollTrigger';
 
-nav();
-darkMode();
-scrollToTop();
+const navItemLinks = document.querySelectorAll('.nav-item');
+const scrollToTopIcon = document.getElementById('scroll-to-top');
+
+toggleDarkMode();
 gsap();
 scrollTrigger();
+
+window.addEventListener('load', () => {
+  setUserPreferedTheme();
+  watchPreferedThemeChange();
+});
+
+navItemLinks.forEach((navItemLink) =>
+  navItemLink.addEventListener('click', function () {
+    closeNavLinks();
+
+    if (window.innerWidth > 640) {
+      navItemLinks.forEach((navItemLink) => navItemLink.classList.remove('active-link'));
+      this.classList.add('active-link');
+    }
+  })
+);
+
+window.addEventListener('scroll', () => {
+  scrollY > 1000
+    ? (scrollToTopIcon.style.display = 'block')
+    : (scrollToTopIcon.style.display = 'none');
+});
+
+scrollToTopIcon.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+window.addEventListener('scroll', handleStickyNav);
+window.addEventListener('resize', closeNavLinks);
