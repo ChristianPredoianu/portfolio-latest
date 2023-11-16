360ad63f82ace6754194fbf7ef116efc59941c79
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-16 11:29:21 +0100

    removed comment

--- js/gsap.js
+++ js/gsap.js
@@ -52,7 +52,7 @@ export default () => {
 
   //Hero section floating icons
   tl.to('.floating-icons__icon', {
-    x: 'random(0, 10)', //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
+    x: 'random(0, 10)',
     y: 'random(-20, 10)',
     duration: 2,
     ease: 'none',
