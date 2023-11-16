25a455a90448cdb3c3d1ef38629727453825c5ea
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-16 14:07:02 +0100

    fixed hamburger bug

--- js/components/nav.js
+++ js/components/nav.js
@@ -11,6 +11,14 @@ export function handleStickyNav() {
     : header.classList.remove('sticky-nav');
 }
 
+function handleActiveLink() {
+  if (header.classList.contains('sticky-nav')) {
+    navItemLinks.forEach((navItemLink) => {
+      navItemLink.style.color = 'var(--clr-light-gray)';
+    });
+  }
+}
+
 function handleStickyNavColor() {
   if (header.classList.contains('sticky-nav')) {
     navLogo.style.color = 'var(--clr-light-gray)';
@@ -27,27 +35,34 @@ function handleStickyNavColor() {
   }
 }
 
-function handleHamburgerColor() {
+function handleHamburgerColorNavOpen() {
   if (navLinks.classList.contains('active')) {
     hamburgerBars.forEach((hamburgerBar) => {
       hamburgerBar.style.backgroundColor = 'hsl(0, 0%, 85%)';
     });
-  } else {
-    hamburgerBars.forEach((hamburgerBar) => {
-      hamburgerBar.style.backgroundColor = '';
-    });
   }
+}
+
+function handleHamburgerColorNavClosed() {
+  hamburgerBars.forEach((hamburgerBar) => {
+    hamburgerBar.style.backgroundColor = 'var(--clr-accent-light';
+  });
 }
 
 export function toggleNavLinks() {
   hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
-  handleHamburgerColor();
+
+  navLinks.classList.contains('active')
+    ? handleHamburgerColorNavOpen()
+    : handleHamburgerColorNavClosed();
 }
 
 export function closeNavLinks() {
   hamburger.classList.remove('active');
   navLinks.classList.remove('active');
+
+  handleHamburgerColorNavClosed();
 }
 
 hamburger.addEventListener('click', toggleNavLinks);
